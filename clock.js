70798c9ea1243a007f9fdd22b71dfcdc8543a09e
dubidubab1234 <lsbmoon78@gmail.com>
2022-07-03 00:22:04 +0900

    edit clock error

--- clock.js
+++ clock.js
@@ -1,9 +1,18 @@
 const clock = document.querySelector(".clock");
 
-setInterval(() => {
+const yoils = ["일", "월", "화", "수", "목", "금", "토"];
+
+function working() {
   const date = new Date();
-  const hour = String(date.getHours()).padStart(2, "0");
-  const minute = String(date.getHours()).padStart(2, "0");
-  const second = String(date.getSeconds()).padStart(2, "0");
-  clock.innerHTML = `${hour}:${minute}:${second}`;
-}, 1000);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let yoil = date.getDay();
+  let hour = String(date.getHours()).padStart(2, "0");
+  let minute = String(date.getMinutes()).padStart(2, "0");
+  let second = String(date.getSeconds()).padStart(2, "0");
+  clock.innerHTML = `${year}. ${month}. ${day}.(${yoils[yoil]}) ${hour}:${minute}:${second}`;
+}
+
+working();
+setInterval(working, 1000);
